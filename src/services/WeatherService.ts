d201d37c32d34465e7a5cd06ce08af86dfbdb21f
Bilal <bilal.hinislioglu@iosb.fraunhofer.de>
2025-04-16 10:30:36 +0200

    a3

---
+++ src/services/WeatherService.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+export const getWeatherByCity = async (city: string) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/weather`, {
+      params: {
+        q: city,
+        appid: API_KEY,
+        units: "metric",
+        lang: "tr",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Hava durumu verisi alınamadı:", error);
+    throw error;
+  }
+};
+
+export const getForecastByCity = async (city: string) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/forecast`, {
+      params: {
+        q: city,
+        appid: API_KEY,
+        units: "metric",
+        lang: "tr",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Hava durumu tahmini alınamadı:", error);
+    throw error;
+  }
+};
